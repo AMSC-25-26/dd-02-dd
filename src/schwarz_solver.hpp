813236b09870275ba6eee3aa9d46ada83a7239db
Alessandro.Russi03 <aler05913@gmail.com>
2025-12-13 10:22:42 +0100

    Add execution header for parallel algorithms

--- src/schwarz_solver.hpp
+++ src/schwarz_solver.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <execution>
 
 // ======================================================
 // ======= TRIDIAGONAL SOLVER - Thomas algorithm ========
