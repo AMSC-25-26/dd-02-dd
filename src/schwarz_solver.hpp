bd546a8cda43d2c35f7a39d3300461423c73d27c
francescapozzi3 <francescamarina.pozzi@mail.polimi.it>
2025-12-13 10:23:56 +0100

    Remove TridiagonalSolver class and related comments
    Removed the TridiagonalSolver class and its documentation.

--- src/schwarz_solver.hpp
+++ src/schwarz_solver.hpp
@@ -6,30 +6,6 @@
 #include <execution>
 
 // ======================================================
-// ======= TRIDIAGONAL SOLVER - Thomas algorithm ========
-// ======================================================
-// 
-// Input:
-//   a   =   sub-diagonal, a[0] not used
-//   b   =   main diagonal
-//   c   =   super-diagonal, c[n-1] not used
-//   r   =   right-hand side vector
-//
-// Output:
-//   x   =   solution vector
-// 
-// Note: all vectors are of size n
-
-class TridiagonalSolver {
-public:
-    static void solve(const std::vector<double> &a,
-                      const std::vector<double> &b,
-                      const std::vector<double> &c,
-                      const std::vector<double> &r,
-                      std::vector<double> &x);
-};
-
-// ======================================================
 // ==================== LOCAL PROBLEM ===================
 // ======================================================
 // 
